3360adedea3326f43a764cbc1f4ffbfc6faa0b90
SutharHet <hetsuthar2001@gmail.com>
2025-01-19 20:40:03 +0530

    initial commit

---
+++ sentry.client.config.ts
@@ -0,0 +1,44 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SANITY_DNS,
+
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+    // Sentry.feedbackIntegration({
+    //   showBranding: false,
+    //   colorScheme: "system",
+    //   triggerLabel: "Leave Feedback",
+    //   triggerAriaLabel: "Leave feedback",
+    //   formTitle: "Feedback",
+    //   submitButtonLabel: "Submit Feedback",
+    //   messagePlaceholder: "What feedback do you have?",
+    //   successMessageText: "Thanks for your feedback!",
+    // }),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  enabled: process.env.NEXT_PUBLIC_SANITY_ENABLED === "true",
+});
